9bc7af6dd2fab2c1d4472b557d436b29a2d0995f
Junior Motta <mottajunior5@gmail.com>
2023-09-05 18:39:01 -0300

    Initial structure

---
+++ src/main.spec.ts
@@ -0,0 +1,7 @@
+describe('Main flow applying differents Design Patterns', () => {
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      expect('Hello World!').toBe('Hello World!');
+    });
+  });
+});
